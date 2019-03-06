c9fb2f26e0e308d41cc144a7839b5efff8d351a3
John Cho <johnycho16@gmail.com>
2019-03-06 03:50:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ def monopoly_with_third_tier
       :names => {},
       :rent_in_dollars => 
       {
-        :one_piece_owned => 25
+        :one_piece_owned => 25,
         :two_pieces_owneed => 50
       }
     }
