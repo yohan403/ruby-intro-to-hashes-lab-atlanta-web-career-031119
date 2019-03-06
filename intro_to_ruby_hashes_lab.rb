d0e734b7731df8f055effc3e1a70dfc98301e9a7
John Cho <johnycho16@gmail.com>
2019-03-06 03:54:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,8 @@ def monopoly_with_third_tier
       :names => 
       {
         :reading_railroad => {},
-        :pennsylvania_railroad => {}
+        :pennsylvania_railroad => {},
+        b_and_o_railroad => {}
       },
       :rent_in_dollars => 
       {
